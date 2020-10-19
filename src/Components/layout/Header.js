de5ae7125edb84be58c9c07a99c8ee3acfdef09f
Vincent Portier <vinz.portier@gmail.com>
2020-10-19 16:08:24 +1100

    created settings menu in the header with logout option

--- src/Components/layout/Header.js
+++ src/Components/layout/Header.js
@@ -6,8 +6,12 @@ import firebase from "firebase";
 //MUI stuff
 import Dialog from "@material-ui/core/Dialog";
 import makeStyles from "@material-ui/core/styles/makeStyles";
+import Menu from "@material-ui/core/Menu";
+
 //Icons
 import AddIcon from "@material-ui/icons/Add";
+import SettingsIcon from "@material-ui/icons/Settings";
+import SignoutIcon from "@material-ui/icons/ExitToApp";
 
 import { useAuthValue, useSelectedProjectValue } from "../../Context/index";
 import { AddTask } from "../tasks/AddTask";
@@ -17,11 +21,33 @@ const useStyles = makeStyles({
   quickAddTaskDialog: {
     padding: 30,
   },
+  settingsMenu: { fontSize: 13, minWidth: 100, outline: "none" },
+  settingsMenu__option: {
+    padding: "4px 10px",
+    display: "flex",
+    alignItems: "center",
+    cursor: "pointer",
+    "&:hover": {
+      background: "#f3f3f3",
+    },
+  },
+  settingsMenu__icon: {
+    color: "grey",
+    fontSize: 18,
+    marginRight: 10,
+  },
+  separator: {
+    height: 1,
+    margin: 4,
+    background: "#ddd",
+  },
 });
 
 export const Header = () => {
   const { setSelectedProject, selectedProject } = useSelectedProjectValue();
   const [showQuickAddTask, setShowQuickAddTask] = useState(false);
+  const [anchorSettingsMenu, setAnchorSettingsMenu] = useState(null);
+  const { user } = useAuthValue();
   const classes = useStyles();
 
   const handleSignout = () => {
@@ -34,6 +60,14 @@ export const Header = () => {
       .catch(function (error) {
         console.log(error);
       });
+  };
+
+  const handleOnSettingsMenuOpen = (event) => {
+    setAnchorSettingsMenu(event.currentTarget);
+  };
+
+  const handleOnSettingsMenuClose = () => {
+    setAnchorSettingsMenu(null);
   };
 
   return (
@@ -57,8 +91,11 @@ export const Header = () => {
                   />
                 </Tooltip>
               </li>
-              <li>
-                <button onClick={handleSignout}>SIGNOUT</button>
+
+              <li onClick={handleOnSettingsMenuOpen}>
+                <Tooltip arrow={true} title="Settings">
+                  <SettingsIcon />
+                </Tooltip>
               </li>
             </ul>
           </div>
@@ -80,6 +117,34 @@ export const Header = () => {
             />
           </div>
         </Dialog>
+        <Menu
+          open={Boolean(anchorSettingsMenu)}
+          onClose={handleOnSettingsMenuClose}
+          anchorEl={anchorSettingsMenu}
+          keepMounted
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center",
+          }}
+        >
+          <div className={classes.settingsMenu}>
+            <div>
+              <span>Welcome {user.name}</span>
+            </div>
+            <div className={classes.separator} />
+            <div
+              className={classes.settingsMenu__option}
+              onClick={handleSignout}
+            >
+              <SignoutIcon className={classes.settingsMenu__icon} />
+              <span>Log out</span>
+            </div>
+          </div>
+        </Menu>
       </div>
     </header>
   );
