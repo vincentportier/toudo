f7a927ec8dc6edab98c813ec0ea1ff4bb7389558
Vincent Portier <vinz.portier@gmail.com>
2020-10-26 17:11:38 +1100

    feature:tutorial for mobiles

--- src/Components/Tutorial/Tutorial.js
+++ src/Components/Tutorial/Tutorial.js
@@ -1,25 +1,9 @@
 import React, { useState, useEffect, useLayoutEffect } from "react";
 import Dialog from "@material-ui/core/Dialog";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-
+import { tutorialOptions } from "../../Constants/index";
 import { useAuthValue } from "../../Context/index";
 import { db } from "../../firebase";
-import createTask from "../../Assets/createTask.gif";
-import addTaskInProject from "../../Assets/addTaskInProject.gif";
-import addTaskInToday from "../../Assets/addTaskInToday.gif";
-import commentOnTask from "../../Assets/commentOnTask.gif";
-import completedTasks from "../../Assets/completedTasks.gif";
-import createProject from "../../Assets/createProject.gif";
-import editTask from "../../Assets/editTask.gif";
-import moreOptions from "../../Assets/moreOptions.gif";
-import quickAddTask from "../../Assets/quickAddTask.gif";
-import reschedule from "../../Assets/reschedule.gif";
-import scheduleTask from "../../Assets/scheduleTask.gif";
-import setPriority from "../../Assets/setPriority.gif";
-import sortByDate from "../../Assets/sortByDate.gif";
-import sortByPriority from "../../Assets/sortByPriority.gif";
-import taskCompleted from "../../Assets/taskCompleted.gif";
-import upcomingTasks from "../../Assets/upcomingTasks.gif";
 
 const useStyles = makeStyles({
   content__optionButtonsContainer: {
@@ -87,8 +71,8 @@ const useStyles = makeStyles({
 export const Tutorial = () => {
   const { user } = useAuthValue();
   const [showTutorial, setShowTutorial] = useState(false);
-  const [width, height] = useWindowSize();
-  const [active, setActive] = useState("");
+  const [width] = useWindowSize();
+  const [active, setActive] = useState("createTask");
   const classes = useStyles();
 
   const handleDismissTutorial = () => {
@@ -109,7 +93,6 @@ export const Tutorial = () => {
   }
 
   useEffect(() => {
-    console.log("listener fired");
     const unsubscribe = db
       .collection("users")
       .doc(user.userId)
@@ -120,123 +103,6 @@ export const Tutorial = () => {
       });
     return () => unsubscribe();
   }, []);
-
-  console.log(width, height);
-
-  const options = [
-    {
-      name: "createTask",
-      header: "Create a task",
-      text: "placeholder instructions",
-      src: createTask,
-      class: "click-to-gif",
-    },
-    {
-      name: "scheduleTask",
-      header: "Schedule a task",
-      text: "placeholder instructions",
-      src: scheduleTask,
-      class: "click-to-gif",
-    },
-    {
-      name: "setPriority",
-      header: "Set the priority",
-      text: "placeholder instructions",
-      src: setPriority,
-      class: "click-to-gif",
-    },
-    {
-      name: "createProject",
-      header: "Create a project",
-      text: "placeholder instructions",
-      src: createProject,
-      class: "click-to-gif",
-    },
-    {
-      name: "addTaskInProject",
-      header: "Add a task in a project",
-      text: "placeholder instructions",
-      src: addTaskInProject,
-      class: "click-to-gif2",
-    },
-    {
-      name: "addTaskInToday",
-      header: "Use the Today view",
-      text: "placeholder instructions",
-      src: addTaskInToday,
-      class: "click-to-gif2",
-    },
-    {
-      name: "upcomingTasks",
-      header: "Use the upcoming view",
-      text: "placeholder instructions",
-      src: upcomingTasks,
-      class: "click-to-gif2",
-    },
-    {
-      name: "taskCompleted",
-      header: "Complete a task",
-      text: "placeholder instructions",
-      src: taskCompleted,
-      class: "click-to-gif2",
-    },
-    {
-      name: "completedTasks",
-      header: "View your completed tasks",
-      text: "placeholder instructions",
-      src: completedTasks,
-      class: "click-to-gif2",
-    },
-    {
-      name: "reschedule",
-      header: "Reschedule outdated tasks",
-      text: "placeholder instructions",
-      src: reschedule,
-      class: "click-to-gif2",
-    },
-    {
-      name: "commentOnTask",
-      header: "Comment on a task",
-      text: "placeholder instructions",
-      src: commentOnTask,
-      class: "click-to-gif2",
-    },
-    {
-      name: "quickAddTask",
-      header: "Use the Quick Add Task",
-      text: "placeholder instructions",
-      src: quickAddTask,
-      class: "click-to-gif2",
-    },
-    {
-      name: "sortByDate",
-      header: "Sort your tasks by date",
-      text: "placeholder instructions",
-      src: sortByDate,
-      class: "click-to-gif2",
-    },
-    {
-      name: "sortByPriority",
-      header: "Sort your tasks by priority",
-      text: "placeholder instructions",
-      src: sortByPriority,
-      class: "click-to-gif2",
-    },
-    {
-      name: "moreOptions",
-      header: "Use the task option menu",
-      text: "placeholder instructions",
-      src: moreOptions,
-      class: "click-to-gif2",
-    },
-    {
-      name: "editTask",
-      header: "Edit a task",
-      text: "placeholder instructions",
-      src: editTask,
-      class: "click-to-gif2",
-    },
-  ];
 
   return (
     <>
@@ -255,7 +121,7 @@ export const Tutorial = () => {
                   filters, share projects, and so much more:
                 </p>
                 <div className={classes.content__optionButtonsContainer}>
-                  {options.map((option) => (
+                  {tutorialOptions.map((option) => (
                     <button
                       className={classes.optionButton}
                       key={option.name}
@@ -267,7 +133,7 @@ export const Tutorial = () => {
                     </button>
                   ))}
                 </div>
-                {options.map((option) => {
+                {tutorialOptions.map((option) => {
                   return (
                     <section key={option.name}>
                       {active === option.name ? (
@@ -309,7 +175,84 @@ export const Tutorial = () => {
         </>
       ) : (
         <Dialog open={showTutorial} onClose={handleDismissTutorial}>
-          Mobile tuto placeholder
+          <div>
+            <div className={classes.dialog__content}>
+              <h2 style={{ margin: 15 }}>Welcome!</h2>
+              <p>
+                Learn the basics by checking out how to create tasks, use
+                filters, share projects, and so much more:
+              </p>
+              <select
+                name="tutorial options"
+                value={active}
+                onChange={(event) => {
+                  setActive(event.target.value);
+                }}
+                style={{
+                  width: "250px",
+                  margin: "auto",
+                  marginTop: "10px",
+                  marginBottom: "10px",
+                }}
+              >
+                {tutorialOptions.map((option) => (
+                  <option value={option.name}>{option.header}</option>
+                ))}
+              </select>
+              {/* <div className={classes.content__optionButtonsContainer}>
+                {tutorialOptions.map((option) => (
+                  <button
+                    className={classes.optionButton}
+                    key={option.name}
+                    onClick={() => {
+                      setActive(option.name);
+                    }}
+                  >
+                    {option.header}
+                  </button>
+                ))}
+              </div> */}
+              {tutorialOptions.map((option) => {
+                return (
+                  <section key={option.name}>
+                    {active === option.name ? (
+                      <>
+                        <h3>{option.header}</h3>
+
+                        <div
+                          style={{
+                            width: "100%",
+
+                            padding: "20px",
+                          }}
+                        >
+                          <label
+                            className={option.classMobile}
+                            title="click/hit space to show gif"
+                          >
+                            <input type="checkbox" />
+                            <img
+                              src={option.srcMobile}
+                              alt={option.header}
+                            ></img>
+                          </label>
+                        </div>
+                      </>
+                    ) : null}
+                  </section>
+                );
+              })}
+
+              <div className={classes.content__footer}>
+                <button
+                  className={classes.confirmButton}
+                  onClick={handleDismissTutorial}
+                >
+                  Got it!
+                </button>
+              </div>
+            </div>
+          </div>
         </Dialog>
       )}
     </>
--- src/Constants/index.js
+++ src/Constants/index.js
@@ -2,6 +2,40 @@ import EmptyInbox from "../EmptyInbox.svg";
 import EmptyToday from "../EmptyToday.svg";
 import EmptyProject from "../EmptyProject.svg";
 
+import createTask from "../Assets/tutorial__desktop/createTask.gif";
+import addTaskInProject from "../Assets/tutorial__desktop/addTaskInProject.gif";
+import addTaskInToday from "../Assets/tutorial__desktop/addTaskInToday.gif";
+import commentOnTask from "../Assets/tutorial__desktop/commentOnTask.gif";
+import completedTasks from "../Assets/tutorial__desktop/completedTasks.gif";
+import createProject from "../Assets/tutorial__desktop/createProject.gif";
+import editTask from "../Assets/tutorial__desktop/editTask.gif";
+import moreOptions from "../Assets/tutorial__desktop/moreOptions.gif";
+import quickAddTask from "../Assets/tutorial__desktop/quickAddTask.gif";
+import reschedule from "../Assets/tutorial__desktop/reschedule.gif";
+import scheduleTask from "../Assets/tutorial__desktop/scheduleTask.gif";
+import setPriority from "../Assets/tutorial__desktop/setPriority.gif";
+import sortByDate from "../Assets/tutorial__desktop/sortByDate.gif";
+import sortByPriority from "../Assets/tutorial__desktop/sortByPriority.gif";
+import taskCompleted from "../Assets/tutorial__desktop/taskCompleted.gif";
+import upcomingTasks from "../Assets/tutorial__desktop/upcomingTasks.gif";
+
+import createTaskMobile from "../Assets/tutorial__mobile/createTask.gif";
+import addTaskInProjectMobile from "../Assets/tutorial__mobile/addTaskInProject.gif";
+import addTaskInTodayMobile from "../Assets/tutorial__mobile/addTaskInToday.gif";
+import commentOnTaskMobile from "../Assets/tutorial__mobile/commentOnTask.gif";
+import completedTasksMobile from "../Assets/tutorial__mobile/completedTasks.gif";
+import createProjectMobile from "../Assets/tutorial__mobile/createProject.gif";
+import editTaskMobile from "../Assets/tutorial__mobile/editTask.gif";
+import moreOptionsMobile from "../Assets/tutorial__mobile/moreOptions.gif";
+import quickAddTaskMobile from "../Assets/tutorial__mobile/quickAddTask.gif";
+import rescheduleMobile from "../Assets/tutorial__mobile/reschedule.gif";
+import scheduleTaskMobile from "../Assets/tutorial__mobile/scheduleTask.gif";
+import setPriorityMobile from "../Assets/tutorial__mobile/setPriority.gif";
+import sortByDateMobile from "../Assets/tutorial__mobile/sortByDate.gif";
+import sortByPriorityMobile from "../Assets/tutorial__mobile/sortByPriority.gif";
+import taskCompletedMobile from "../Assets/tutorial__mobile/taskCompleted.gif";
+import upcomingTasksMobile from "../Assets/tutorial__mobile/upcomingTasks.gif";
+
 export const collatedTasks = [
   { key: "INBOX", name: "Inbox" },
   { key: "UPCOMING", name: "Upcoming" },
@@ -45,5 +79,152 @@ export const emptyState = [
     header: "What will you accomplish",
     message: "",
     imageSource: EmptyProject,
+  },
+];
+
+export const tutorialOptions = [
+  {
+    name: "createTask",
+    header: "Create a task",
+    text: "placeholder instructions",
+    src: createTask,
+    srcMobile: createTaskMobile,
+    class: "click-to-gif",
+    classMobile: "click-to-gif-mobile",
+  },
+  {
+    name: "scheduleTask",
+    header: "Schedule a task",
+    text: "placeholder instructions",
+    src: scheduleTask,
+    srcMobile: scheduleTaskMobile,
+    class: "click-to-gif",
+    classMobile: "click-to-gif-mobile",
+  },
+  {
+    name: "setPriority",
+    header: "Set the priority",
+    text: "placeholder instructions",
+    src: setPriority,
+    srcMobile: setPriorityMobile,
+    class: "click-to-gif",
+    classMobile: "click-to-gif-mobile",
+  },
+  {
+    name: "createProject",
+    header: "Create a project",
+    text: "placeholder instructions",
+    src: createProject,
+    srcMobile: createProjectMobile,
+    class: "click-to-gif",
+    classMobile: "click-to-gif-mobile",
+  },
+  {
+    name: "addTaskInProject",
+    header: "Add a task in a project",
+    text: "placeholder instructions",
+    src: addTaskInProject,
+    srcMobile: addTaskInProjectMobile,
+    class: "click-to-gif2",
+    classMobile: "click-to-gif-mobile",
+  },
+  {
+    name: "addTaskInToday",
+    header: "Use the Today view",
+    text: "placeholder instructions",
+    src: addTaskInToday,
+    srcMobile: addTaskInTodayMobile,
+    class: "click-to-gif2",
+    classMobile: "click-to-gif-mobile",
+  },
+  {
+    name: "upcomingTasks",
+    header: "Use the upcoming view",
+    text: "placeholder instructions",
+    src: upcomingTasks,
+    srcMobile: upcomingTasksMobile,
+    class: "click-to-gif2",
+    classMobile: "click-to-gif-mobile",
+  },
+  {
+    name: "taskCompleted",
+    header: "Complete a task",
+    text: "placeholder instructions",
+    src: taskCompleted,
+    srcMobile: taskCompletedMobile,
+    class: "click-to-gif2",
+    classMobile: "click-to-gif-mobile",
+  },
+  {
+    name: "completedTasks",
+    header: "View your completed tasks",
+    text: "placeholder instructions",
+    src: completedTasks,
+    srcMobile: completedTasksMobile,
+    class: "click-to-gif2",
+    classMobile: "click-to-gif-mobile",
+  },
+  {
+    name: "reschedule",
+    header: "Reschedule outdated tasks",
+    text: "placeholder instructions",
+    src: reschedule,
+    srcMobile: rescheduleMobile,
+    class: "click-to-gif2",
+    classMobile: "click-to-gif-mobile",
+  },
+  {
+    name: "commentOnTask",
+    header: "Comment on a task",
+    text: "placeholder instructions",
+    src: commentOnTask,
+    srcMobile: commentOnTaskMobile,
+    class: "click-to-gif2",
+    classMobile: "click-to-gif-mobile",
+  },
+  {
+    name: "quickAddTask",
+    header: "Use the Quick Add Task",
+    text: "placeholder instructions",
+    src: quickAddTask,
+    srcMobile: quickAddTaskMobile,
+    class: "click-to-gif2",
+    classMobile: "click-to-gif-mobile",
+  },
+  {
+    name: "sortByDate",
+    header: "Sort your tasks by date",
+    text: "placeholder instructions",
+    src: sortByDate,
+    srcMobile: sortByDateMobile,
+    class: "click-to-gif2",
+    classMobile: "click-to-gif-mobile",
+  },
+  {
+    name: "sortByPriority",
+    header: "Sort your tasks by priority",
+    text: "placeholder instructions",
+    src: sortByPriority,
+    srcMobile: sortByPriorityMobile,
+    class: "click-to-gif2",
+    classMobile: "click-to-gif-mobile",
+  },
+  {
+    name: "moreOptions",
+    header: "Use the task option menu",
+    text: "placeholder instructions",
+    src: moreOptions,
+    srcMobile: moreOptionsMobile,
+    class: "click-to-gif2",
+    classMobile: "click-to-gif-mobile",
+  },
+  {
+    name: "editTask",
+    header: "Edit a task",
+    text: "placeholder instructions",
+    src: editTask,
+    srcMobile: editTaskMobile,
+    class: "click-to-gif2",
+    classMobile: "click-to-gif-mobile",
   },
 ];
